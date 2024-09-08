3a8714819eb20f278061cc105a33791d649c3d05
Zirins <BensonLin80169@gmail.com>
2024-09-07 23:57:10 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Welcome to My Movie Analysis Site</h1>
+        <p>This is where I will post movie reviews and analyses.</p>
+      </div>
   );
 }
 
